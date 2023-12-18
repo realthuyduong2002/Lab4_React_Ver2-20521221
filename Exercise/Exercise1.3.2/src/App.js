ce4605128c512beb0f62429a7216a45afcc50480
realthuyduong2002 <20521221@gm.uit.edu.vn>
2023-12-18 11:36:08 +0700

    commit

---
+++ Exercise/Exercise1.3.2/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter,Link,Route, Switch } from 'react-router-dom';
+import PeopleContainer from './PeopleContainer'
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/people'>
+          <PeopleContainer/>
+        </Route>
+        <Link to='/people'>People</Link>
+      </Switch>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
